65419cbdde8899de42fa7cf88cee78a7be318860
tk3 <tk3inc@gmail.com>
2021-01-12 21:24:51 +0900

    add Game project and SDL project

---
+++ Game3/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open MySDL2
+
+[<EntryPoint>]
+let main argv =
+    MySDL2.Say.hello("foo")
+    printfn "Hello World from F#!"
+    0 // return an integer exit code

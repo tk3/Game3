ca096c53e49a5c7ea83fb3ed18f106ba9b8ae145
tk3 <tk3inc@gmail.com>
2021-01-12 23:07:19 +0900

    update

--- Game3/Program.fs
+++ Game3/Program.fs
@@ -1,10 +1,14 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open MySDL2
+﻿open MySDL2
 
 [<EntryPoint>]
 let main argv =
-    MySDL2.Say.hello("foo")
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let SDL_INIT_VIDEO = 0x00000020u
+    let _ = Api.SDL_Init(SDL_INIT_VIDEO)
+
+    let SDL_WINDOWPOS_CENTERED_MASK = 0x2FFF0000
+    let SDL_WINDOWPOS_CENTERED_DISPLAY x = SDL_WINDOWPOS_CENTERED_MASK ||| x
+    let SDL_WINDOWPOS_CENTERED = SDL_WINDOWPOS_CENTERED_DISPLAY 0
+
+    let _ = Api.SDL_CreateWindow("Game3", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0u)
+
+    0
--- MySDL2/Library.fs
+++ MySDL2/Library.fs
@@ -1,5 +1,18 @@
 ﻿namespace MySDL2
 
-module Say =
+open System
+open System.Runtime.InteropServices
+
+module Api =
+    // Uint32 ?
+
+    // SDL.h
+    [<DllImport("SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
+    extern int SDL_Init(uint32 flags)
+
+    // SDL_video.h
+    [<DllImport("SDL2.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)>]
+    extern IntPtr SDL_CreateWindow(string title, int x, int y, int w, int h, uint32 flags)
+
     let hello name =
         printfn "Hello %s" name
